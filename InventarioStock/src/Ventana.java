ac651ad253c929ed989774c81aabc90b98fdb25b
Steadeezy <Steadeezy@gmail.com>
2022-11-03 16:07:29 -0300

    Parte de la interfaz

---
+++ InventarioStock/src/Ventana.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Ventana {
+    private JTextField textField1;
+    private JPanel panel1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton actualizarButton;
+    private JTable table1;
+    private JButton retirarButton;
+    private JButton agregarButton;
+    private JButton verInformeButton;
+}
